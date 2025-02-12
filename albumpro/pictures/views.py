f007a8b537903bc143a8b870a6d530aa991d9d96
Stefano <tafeupatrick2@gmail.com>
2025-02-12 21:12:28 +0200

    merging change from event and update files

--- albumpro/pictures/views.py
+++ albumpro/pictures/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Categories, Photo
 
@@ -6,16 +6,51 @@ from .models import Categories, Photo
 def venu(request):
   return HttpResponse("I am Testing my git branch! so i hope it is going to work out!")
 
+
+#la section des categories ici********************
 def galery(request):
+     categori = request.GET.get('categori')
+     if  categori == None:
+           photos = Photo.objects.all()
+     else:
+           photos = Photo.objects.filter(categories__name=categori)
+
+
      categories = Categories.objects.all()
-     photos = Photo.objects.all()
+
      context = {'categories':categories,'photos':photos}
     
      return render(request, 'pictures/galery.html', context)
 
+#la section deds ajouts des images ici****************
 def ajouter(request):
-   return render(request, 'pictures/ajouter.html')
+   categories = Categories.objects.all()
 
+   if request.method == 'POST':
+      data = request.POST
+      image = request.FILES.get('image')
+      
+      if data['categories'] != 'none':
+         categories = Categories.objects.get(id=data['categories'])
+      elif data['creer'] != '':
+         categories, created = Categories.objects.get_or_create(name=data['creer'])
+      else:
+         categories = None
+
+      photos = Photo.objects.create(
+         categories = categories,
+         desciption= data['desciption'],
+         image=image,
+      )
+
+      return redirect('galery')
+  
+
+
+   context = {'categories':categories}
+   return render(request, 'pictures/ajouter.html', context)
+
+#la section des photos ici******************************
 def photo(request, pk):
    photos = Photo.objects.get(id=pk)  # we use 'get' in case we wanna get y url
    context = {'photos':photos}
