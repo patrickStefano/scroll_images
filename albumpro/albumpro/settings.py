56c8cf6d5ad112872c6c38a22362a8c593d75eb9
Stefano <tafeupatrick2@gmail.com>
2025-02-15 13:13:40 +0200

    Adding a title

--- albumpro/albumpro/settings.py
+++ albumpro/albumpro/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+# import dj_database_url
+# import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +133,4 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
